0dc5ed5148e67a1347adce6a8175bc6b01df81f8
Maaz Quraishi <maazquraishi@JHS.LOCAL>
2026-05-31 22:33:53 +0530

    Fix MongoDB docker configuration

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -4,7 +4,7 @@ class Settings(BaseSettings):
     mongodb_url: str
     database_name: str = "JHS_website"
 
-    class Config:
+    class Config:  
         env_file = ".env"
 
 settings = Settings()

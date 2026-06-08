4158b21d0df596f6a41784652487dc7c21a33efc
Maaz Quraishi <maazquraishi@JHS.LOCAL>
2026-06-08 20:05:19 +0530

    Update para color and card bg

--- frontend/src/components/sectors/Media & technology/ITTeS.tsx
+++ frontend/src/components/sectors/Media & technology/ITTeS.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { ArrowUpRight, Code2, FileText, ShieldCheck, BarChart2, Scale, Globe2 } from 'lucide-react'
-import './ITTeS.css'
+import './ITTeS.css'  
 
 gsap.registerPlugin(ScrollTrigger)
 

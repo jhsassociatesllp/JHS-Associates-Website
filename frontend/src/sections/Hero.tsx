3c6955b2a7b7009b94d0f523e5841d9d39aa7fce
Maaz Quraishi <maazquraishi@JHS.LOCAL>
2026-06-29 19:10:36 +0530

    update Partner Details and sections

--- frontend/src/sections/Hero.tsx
+++ frontend/src/sections/Hero.tsx
@@ -61,7 +61,7 @@ const cards: Card[] = [
   },
   {
     id: 5,
-    image: imageUrl('card6.png'),
+    image: imageUrl('Card6.png'),
     category: "DIGITAL",
     meta: "AI & Automation • APRIL 5, 2026",
     title: "Digital Twins: The Future of Industrial Operations",
